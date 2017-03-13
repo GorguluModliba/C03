eeda540708c6256efdadf6faecf53724783c1172
ygoerguelue <ygoerguelue@student.tgm.ac.at>
2017-03-13 10:46:42 +0100

    V02

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,10 +3,15 @@ public class HelloWorld {
 
 	public static void main(String[] args){
 		dmodliba();
+		ygoerguelue();
 	}
 	
 	public static void dmodliba(){
 		System.out.println("Schöne Grüße von Daniel Modliba!");
+	}
+	
+	public static void ygoerguelue(){
+		System.out.print("Schöne Grüße von Yasin Görgülü");
 	}
 	
 }
